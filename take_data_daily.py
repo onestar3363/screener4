c4cc8d9ad52550433d36005335430ba2fc14348d
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-12-22 23:58:04 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -382,8 +382,8 @@ for name, frame,framew in zip(names,framelist,framelistw):
                    #         sira +=1
                    #         expander('week pullback')                             
                 if option2 == 'pullback':  
-                   if (frame['Decision Super'].iloc[-h1]=='Buy2' or frame['Decision Super2'].iloc[-h1]=='Buy2' or frame['Decision Super3'].iloc[-h1]=='Buy2'\
-                   or frame['EMA50_cross'].iloc[-h1]=='Buy2' or frame['EMA20_cross'].iloc[-h1]=='Buy2' or frame['EMA200_cross'].iloc[-h1]=='Buy2')\
+                   if (frame['Decision Super'].iloc[-h1-1]=='Buy2' or frame['Decision Super2'].iloc[-h1-1]=='Buy2' or frame['Decision Super3'].iloc[-h1-1]=='Buy2'\
+                   or frame['EMA50_cross'].iloc[-h1-1]=='Buy2' or frame['EMA20_cross'].iloc[-h1-1]=='Buy2' or frame['EMA200_cross'].iloc[-h1-1]=='Buy2')\
                    and frame['Close'].iloc[-h1]>frame['sup4'].iloc[-h1] and frame['Close'].iloc[-h1]>frame['sup6'].iloc[-h1] \
                    and (frame['Dec_EMA50'].iloc[-h1]=='Buy' or frame['Dec_EMA20'].iloc[-h1]=='Buy')\
                    and frame['Dec_EMA200'].iloc[-h1]=='Buy'\
