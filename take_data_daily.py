ff23d9bbf4ce40284d493c3c7023b6884cd43229
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-12-26 00:54:23 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -401,7 +401,6 @@ for name, frame,framew in zip(names,framelist,framelistw):
                    and (frame['Dec_EMA50'].iloc[-h1]=='Buy'and frame['Dec_EMA20'].iloc[-h1]=='Buy')\
                    and (frame['Close'].iloc[-h1]>frame['sup2'].iloc[-h1]>frame['sup4'].iloc[-h1]>frame['sup6'].iloc[-h1])\
                    and frame['Dec_EMA200'].iloc[-h1]=='Buy'\
-                   and frame['Trend MACD'].iloc[-h1]=='Buy'\
                    and frame['Dec_MACD'].iloc[-h1]=='Buy':
                    #and frame['Close'].iloc[-h1]<1.015*frame['sup2'].iloc[-h1]\
                    #and frame['MACD'].iloc[-h1]>frame['MACD'].iloc[-h1-1]\
