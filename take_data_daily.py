d392fb269a1245797cda269fe25a381eccd33134
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-12-28 16:45:17 +0300

    Update take_data_daily.py

--- take_data_daily.py
+++ take_data_daily.py
@@ -416,12 +416,14 @@ for name, frame, frameh, framew in zip(names,framelist, framelisth, framelistw):
                    #         sira +=1
                    #         expander('week pullback')                             
                 if option2 == 'pullback':  
-                   if (frame['Decision Super'].iloc[-h1-1]=='Buy2' or frame['Decision Super2'].iloc[-h1-1]=='Buy2' or frame['Decision Super3'].iloc[-h1-1]=='Buy2'\
-                   or frame['EMA50_cross'].iloc[-h1-1]=='Buy2' or frame['EMA20_cross'].iloc[-h1-1]=='Buy2' or frame['EMA200_cross'].iloc[-h1-1]=='Buy2')\
+                   if (frame['Decision Super'].iloc[-h1]=='Buy2' or frame['Decision Super2'].iloc[-h1]=='Buy2' or frame['Decision Super3'].iloc[-h1]=='Buy2'\
+                   or frame['EMA50_cross'].iloc[-h1]=='Buy2' or frame['EMA20_cross'].iloc[-h1]=='Buy2' or frame['EMA200_cross'].iloc[-h1]=='Buy2')\
                    and (frame['Close'].iloc[-h1]>frame['sup4'].iloc[-h1] and frame['Close'].iloc[-h1]>frame['sup6'].iloc[-h1])\
                    and (frame['Dec_EMA50'].iloc[-h1]=='Buy' or frame['Dec_EMA20'].iloc[-h1]=='Buy')\
                    and frame['Dec_EMA200'].iloc[-h1]=='Buy'\
-                   and frame['Close'].iloc[-h1]>frame['Open'].iloc[-h1]:
+                   and (frameh['Close'].iloc[-h1]>frameh['sup4'].iloc[-h1] or frameh['Close'].iloc[-h1]>frameh['sup6'].iloc[-h1]\
+                   or frameh['Dec_EMA50'].iloc[-h1]=='Buy' or frameh['Dec_EMA20'].iloc[-h1]=='Buy'):
+                   #and frame['Close'].iloc[-h1]>frame['Close'].iloc[-h1-1]:
                    #and (frame['Dec_MACD'].iloc[-h1]=='Buy' or frame['Trend MACD'].iloc[-h1]=='Buy'):
                    #and frame['Close'].iloc[-h1]>frame['sup4'].iloc[-h1] and frame['Close'].iloc[-h1]>frame['sup6'].iloc[-h1] \
                    #and frame['Close'].iloc[-1]>frame['Close'].iloc[-2]\
