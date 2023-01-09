9e2d5aa3f377f01fb27fff0f62c01415182244ba
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2023-01-09 10:08:47 +0300

    Update dataframes.py

--- dataframes.py
+++ dataframes.py
@@ -122,7 +122,6 @@ def connect_engine(url):
 def connect_enginew(url):
     enginew=sqlalchemy.create_engine(url) 
     return enginew
-start = time.perf_counter()
 
 def get_names():
     names= pd.read_sql('SELECT name FROM sqlite_master WHERE type="table"',engine)
