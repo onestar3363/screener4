1a1c5a26d36d30bbdaf5db611ec37d58e2415dc7
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2023-01-09 09:52:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,16 @@ if __name__ == '__main__':
     st.set_page_config(layout="wide")
     st.title('Screener')
     getdata1.getdata()
+    connection_url='sqlite:///günlük.db'
+    connection_url2='sqlite:///haftalik.db'
+    engine= connect_engine(connection_url) 
+    enginew= connect_enginew(connection_url2)
+    start = time.perf_counter()
+    names=get_names()
+    framelist=get_framelist()
+    framelistw=get_framelistw()
+    end = time.perf_counter()
+    st.write(end - start)
     
     
     
