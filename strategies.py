189ef6c8400735032b57c01cae5452e503ab53c3
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2023-01-09 13:36:52 +0300

    Update strategies.py

--- strategies.py
+++ strategies.py
@@ -25,6 +25,25 @@ framelist=dataframes.get_framelist()
 framelistw=dataframes.get_framelistw()
 
 
+def expander(cond):
+       with st.expander(cond+' '+str(sira) +') '+ name+'/'+' RISK= '+str(frame['RISK'].iloc[-1].round(2))+'/ %ATR='+str(frame['ATR%'].iloc[-1].round(2))+'/ %wATR='+str(framew['ATR%'].iloc[-1].round(2))):
+           #st.write(str(sira) +') '+ name+'/'+' RISK= '+str(frame['RISK'].iloc[-1].round(2))+'/ %ATR='+str(frame['ATR%'].iloc[-1].round(2)))
+           #col6, col3, col4 = st.columns([1, 1, 1])
+           col3, col4 = st.columns([1, 1])
+           col3.write(frame[['Close','ATR%','ADX','EMA20_cross','EMA50_cross','Decision Super','Decision Super2','Decision Super3','Dec_MACD','Trend MACD','MACD_diff']].tail(2))
+           col4.write(framew[['Close','ATR%','ADX','EMA20_cross','EMA50_cross','Decision Super','Decision Super2','Decision Super3','Dec_MACD','Trend MACD','MACD_diff']].tail(2))
+           #col6.write(frameh[['Close','ATR%','ADX','Dec_EMA50','Dec_MACD','Trend MACD','MACD_diff']].tail(2))
+           #col5, col1, col2 = st.columns([1, 1, 1])
+           col1, col2 = st.columns([1, 1])
+           r=100
+           fig=get_figures(frame,r)
+           r=40
+           figw=get_figures(framew,r)
+           r=300      
+           col1.plotly_chart(fig,use_container_width=True)
+           col2.plotly_chart(figw,use_container_width=True)
+
+
 def strategy(adx_value,adx_value2,h1,option1,option2,option3,sira):
     for name, frame, framew in zip(names,framelist, framelistw): 
         try:
@@ -41,7 +60,7 @@ def strategy(adx_value,adx_value2,h1,option1,option2,option3,sira):
                        or frame['EMA50_cross'].iloc[-h1]=='Buy' or frame['EMA50_cross'].iloc[-h1]=='Buy')\
                        and (frame['Close'].iloc[-h1]>frame['sup4'].iloc[-h1] or frame['Close'].iloc[-h1]>frame['sup6'].iloc[-h1]):
                                 sira +=1
-                                expander()
+                                expander('breakout')
                                 
                     if option2 == 'pullback':  
                        if (frame['Decision Super2'].iloc[-h1]=='Buy2' or frame['Decision Super2'].iloc[-h1]=='Buy2' or frame['Decision Super3'].iloc[-h1]=='Buy2'\
