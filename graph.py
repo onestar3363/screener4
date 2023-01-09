6db58fdb39443d2be0fb8dd8d30b4392391989d3
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2023-01-09 13:19:05 +0300

    Update graph.py

--- graph.py
+++ graph.py
@@ -77,7 +77,7 @@ def get_figures(frame,r):
     return fig
 
 
-def expander(sira,name,frame,framew,fig,figw):
+def expander():
        with st.expander(str(sira) +') '+ name+'/'+' RISK= '+str(frame['RISK'].iloc[-1].round(2))+'/ %ATR='+str(frame['ATR%'].iloc[-1].round(2))+'/ %wATR='+str(framew['ATR%'].iloc[-1].round(2))):
            #st.write(str(sira) +') '+ name+'/'+' RISK= '+str(frame['RISK'].iloc[-1].round(2))+'/ %ATR='+str(frame['ATR%'].iloc[-1].round(2)))
            #col6, col3, col4 = st.columns([1, 1, 1])
