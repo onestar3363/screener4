a47afb37217cb91d727bb8aa79c66b9bc6eb89c7
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2023-01-09 08:43:58 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+import streamlit as st
+import yfinance as yf
+from PIL import Image
+import requests
+from io import BytesIO
+import financial
+import stockchart
+import backtest
 
+
+if __name__ == '__main__':
+
+    st.title('Trading DashBoard')
+    url = 'https://g.foolcdn.com/editorial/images/602904/why-tesla-stock-is-up-today.jpg'
+    response = requests.get(url)
+    img = Image.open(BytesIO(response. content))
