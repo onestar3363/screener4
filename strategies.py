23c42d347624a3c2b1ac1b8a4196af6f27954e08
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2023-01-09 13:46:27 +0300

    Update strategies.py

--- strategies.py
+++ strategies.py
@@ -25,23 +25,7 @@ framelist=dataframes.get_framelist()
 framelistw=dataframes.get_framelistw()
 
 
-def expander(option2,sira):
-       with st.expander(option2+' '+str(sira) +') '+ name+'/'+' RISK= '+str(frame['RISK'].iloc[-1].round(2))+'/ %ATR='+str(frame['ATR%'].iloc[-1].round(2))+'/ %wATR='+str(framew['ATR%'].iloc[-1].round(2))):
-           #st.write(str(sira) +') '+ name+'/'+' RISK= '+str(frame['RISK'].iloc[-1].round(2))+'/ %ATR='+str(frame['ATR%'].iloc[-1].round(2)))
-           #col6, col3, col4 = st.columns([1, 1, 1])
-           col3, col4 = st.columns([1, 1])
-           col3.write(frame[['Close','ATR%','ADX','EMA20_cross','EMA50_cross','Decision Super','Decision Super2','Decision Super3','Dec_MACD','Trend MACD','MACD_diff']].tail(2))
-           col4.write(framew[['Close','ATR%','ADX','EMA20_cross','EMA50_cross','Decision Super','Decision Super2','Decision Super3','Dec_MACD','Trend MACD','MACD_diff']].tail(2))
-           #col6.write(frameh[['Close','ATR%','ADX','Dec_EMA50','Dec_MACD','Trend MACD','MACD_diff']].tail(2))
-           #col5, col1, col2 = st.columns([1, 1, 1])
-           col1, col2 = st.columns([1, 1])
-           r=100
-           fig=get_figures(frame,r)
-           r=40
-           figw=get_figures(framew,r)
-           r=300      
-           col1.plotly_chart(fig,use_container_width=True)
-           col2.plotly_chart(figw,use_container_width=True)
+
 
 
 def strategy(adx_value,adx_value2,h1,option1,option2,option3,sira):
@@ -143,4 +127,21 @@ def strategy(adx_value,adx_value2,h1,option1,option2,option3,sira):
         except Exception as e:
             st.write(name,e) 
         
+ def expander(option2,sira):
+       with st.expander(option2+' '+str(sira) +') '+ name+'/'+' RISK= '+str(frame['RISK'].iloc[-1].round(2))+'/ %ATR='+str(frame['ATR%'].iloc[-1].round(2))+'/ %wATR='+str(framew['ATR%'].iloc[-1].round(2))):
+           #st.write(str(sira) +') '+ name+'/'+' RISK= '+str(frame['RISK'].iloc[-1].round(2))+'/ %ATR='+str(frame['ATR%'].iloc[-1].round(2)))
+           #col6, col3, col4 = st.columns([1, 1, 1])
+           col3, col4 = st.columns([1, 1])
+           col3.write(frame[['Close','ATR%','ADX','EMA20_cross','EMA50_cross','Decision Super','Decision Super2','Decision Super3','Dec_MACD','Trend MACD','MACD_diff']].tail(2))
+           col4.write(framew[['Close','ATR%','ADX','EMA20_cross','EMA50_cross','Decision Super','Decision Super2','Decision Super3','Dec_MACD','Trend MACD','MACD_diff']].tail(2))
+           #col6.write(frameh[['Close','ATR%','ADX','Dec_EMA50','Dec_MACD','Trend MACD','MACD_diff']].tail(2))
+           #col5, col1, col2 = st.columns([1, 1, 1])
+           col1, col2 = st.columns([1, 1])
+           r=100
+           fig=get_figures(frame,r)
+           r=40
+           figw=get_figures(framew,r)
+           r=300      
+           col1.plotly_chart(fig,use_container_width=True)
+           col2.plotly_chart(figw,use_container_width=True)
     
