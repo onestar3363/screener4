5e8cb50325c40e15c782f84cadfde33473b7a9aa
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2023-01-09 23:13:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,30 +15,25 @@ import dataframes
 import strategies
 
 
-@st.cache(suppress_st_warning=True)
-def app():
+st.title('Screener')
+dataframes.getdata()
+start = time.perf_counter()  
+names=dataframes.get_names()
+framelist=dataframes.get_framelist()
+framelistw=dataframes.get_framelistw()
+end = time.perf_counter()
+st.write(end - start)
+sira=0
+option1 = st.sidebar.selectbox("Buy or Sell",('Buy','Sell')) 
+option2 = st.sidebar.selectbox("Which Indicator?", ('breakout','pullback','consolidating','week','EMASUPER','Index','EMA50','Supertrend','EMA20','MACD','ADX','EMA200'))
+adx_value= st.sidebar.number_input('ADX Value',min_value=10,value=18)
+adx_value2= st.sidebar.number_input('ADX Value_ust',min_value=10,value=60)
+h=st.sidebar.number_input('Geçmiş',value=1)
+h1=int(h)
+riskvalue=st.sidebar.number_input('Risk',min_value=1,value=1000)
+option3=st.sidebar.text_input('Ticker','')
+fark=st.sidebar.number_input('Fark',min_value=1.0,value=5.0,step=0.5)
+st.header(option1 + option2)
+indices=['US500/USD_S&P 500_INDEX_US','EU50/EUR_Euro Stoxx 50_INDEX_DE','^N225','XU030.IS']
+strategies.strategy(adx_value,adx_value2,h1,option1,option2,option3,sira,names,framelist,framelistw)
 
-    st.title('Screener')
-    dataframes.getdata()
-    start = time.perf_counter()  
-    names=dataframes.get_names()
-    framelist=dataframes.get_framelist()
-    framelistw=dataframes.get_framelistw()
-    end = time.perf_counter()
-    st.write(end - start)
-    
-    sira=0
-    option1 = st.sidebar.selectbox("Buy or Sell",('Buy','Sell')) 
-    option2 = st.sidebar.selectbox("Which Indicator?", ('breakout','pullback','consolidating','week','EMASUPER','Index','EMA50','Supertrend','EMA20','MACD','ADX','EMA200'))
-    adx_value= st.sidebar.number_input('ADX Value',min_value=10,value=18)
-    adx_value2= st.sidebar.number_input('ADX Value_ust',min_value=10,value=60)
-    h=st.sidebar.number_input('Geçmiş',value=1)
-    h1=int(h)
-    riskvalue=st.sidebar.number_input('Risk',min_value=1,value=1000)
-    option3=st.sidebar.text_input('Ticker','')
-    fark=st.sidebar.number_input('Fark',min_value=1.0,value=5.0,step=0.5)
-    st.header(option1 + option2)
-    indices=['US500/USD_S&P 500_INDEX_US','EU50/EUR_Euro Stoxx 50_INDEX_DE','^N225','XU030.IS']
-    strategies.strategy(adx_value,adx_value2,h1,option1,option2,option3,sira,names,framelist,framelistw)
-if __name__ == "__main__":
-    app()
