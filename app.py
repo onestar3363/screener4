b10949f564515c150ee22fe9a625bb2a1b37a514
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2023-01-09 09:04:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 import streamlit as st
+import pandas as pd
+import sqlalchemy
+import ta
+import numpy as np
 import yfinance as yf
-from PIL import Image
-import requests
-from io import BytesIO
-import financial
-import stockchart
-import backtest
-
+import sqlalchemy
+import time
+import pandas_ta as pa
+import os
+import plotly
+import plotly.graph_objs as go 
+import base64
+import getdata
 
 if __name__ == '__main__':
 
-    st.title('Trading DashBoard')
-    url = 'https://g.foolcdn.com/editorial/images/602904/why-tesla-stock-is-up-today.jpg'
-    response = requests.get(url)
-    img = Image.open(BytesIO(response. content))
+    st.set_page_config(layout="wide")
+    st.title('Screener')
+    start = time.perf_counter()
+    lastindex=getdata.getdata()
+    end = time.perf_counter() 
+    st.write('Last downloaded', lastindex, 'Süre', end - start)
+    
+    
+    
